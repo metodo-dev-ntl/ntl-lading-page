5761b96d13e00bde91cf8e856da7a9ba9c1f8621
lidierynascimento <lidierynascimento@users.noreply.github.com>
2026-08-18 14:33:48 -0300

    content: disable Meta Pixel when env=develop

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,7 +101,9 @@ function RootShell({ children }: { children: ReactNode }) {
         {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
+            __html: `(function(){
+if(new URLSearchParams(window.location.search).get('env')==='develop')return;
+!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -110,7 +112,7 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '2104673067074414');
-fbq('track', 'PageView');`,
+fbq('track', 'PageView');})();`,
           }}
         />
         <noscript>
